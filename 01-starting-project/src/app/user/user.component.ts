fbd7b5532d5335c80c48ce3eb28f61389b4a000a
hshoon <hshoon@egiskorea.com>
2025-03-24 15:18:39 +0900

    @output으로 부모 컴포넌트에 값 전달달

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, computed} from '@angular/core';
+import { Component, Input, input, computed, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,18 +8,21 @@ import { Component, Input, input, computed} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true}) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
+    this.select.emit(this.id);
   }
 }
