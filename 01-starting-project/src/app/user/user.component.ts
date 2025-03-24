4b11542c1b2ad232b9eee384c2bf4f0fcc650e10
hohshho <tkdgur8377@gmail.com>
2025-03-24 17:38:00 +0900

    task 생성, model 추가

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component, Input, input, computed, Output, EventEmitter, output} from '@angular/core';
 
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
