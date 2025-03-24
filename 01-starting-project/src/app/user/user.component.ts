47f72116f11c805a1b002d76f0c9f7cd8e1b0316
hshoon <hshoon@egiskorea.com>
2025-03-24 16:40:27 +0900

    typescript 객체 적용용

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -8,9 +8,11 @@ import { Component, Input, input, computed, Output, EventEmitter, output} from '
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true}) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true}) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  }
   @Output() select = new EventEmitter();
   // avatar = input.required<string>();
   // name = input.required<string>();
@@ -19,10 +21,10 @@ export class UserComponent {
   //   return 'assets/users/' + this.avatar();
   // });
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
