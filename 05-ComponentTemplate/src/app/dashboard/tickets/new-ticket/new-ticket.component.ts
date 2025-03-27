5afbb1b223d9c2c006f732b2703e9de50e54187b
hshoon <hshoon@egiskorea.com>
2025-03-28 08:56:00 +0900

    viewChild사용해 form객체 다루기기

--- 05-ComponentTemplate/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 05-ComponentTemplate/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
   onSubmit(title: string, ticketText: string) {
     console.log(title + " " + ticketText);
+    this.form?.nativeElement.reset();
   }
 }
