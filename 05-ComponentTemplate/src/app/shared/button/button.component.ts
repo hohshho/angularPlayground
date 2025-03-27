3d9be33999ce4ebe090e1507cfc29401914a0efb
hshoon <hshoon@egiskorea.com>
2025-03-27 11:23:07 +0900

    host요소에 class추가

--- 05-ComponentTemplate/src/app/shared/button/button.component.ts
+++ 05-ComponentTemplate/src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[app-button], a[appButton]',
+  selector: 'button[app-button], a[app-button]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
