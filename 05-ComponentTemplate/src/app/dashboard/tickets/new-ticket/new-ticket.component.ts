7f1818f47852b910b6ab519ba9443d1c58465b70
hohshho <tkdgur8377@gmail.com>
2025-03-28 11:07:50 +0900

    AfterViewInit, AfterContentInit 라이프사이클 함수 확인인

--- 05-ComponentTemplate/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 05-ComponentTemplate/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -10,11 +10,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit{
   @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit() {
+    console.log('ONINIT')
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT')
+    console.log(this.form?.nativeElement);
+  }
 
   onSubmit(title: string, ticketText: string) {
     console.log(title + " " + ticketText);
     this.form?.nativeElement.reset();
+    console.log(this.form);
   }
 }
