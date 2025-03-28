2b458a26899e3e80936474d7801323e40b4f7b02
hshoon <hshoon@egiskorea.com>
2025-03-28 17:13:34 +0900

    기능추가

--- 05-ComponentTemplate/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 05-ComponentTemplate/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -9,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class TicketComponent {
 
+  data = input.required<Ticket>();
+  close = output();
+  detailIsVisible = signal(false);
+
+  onToggleDetail() {
+    // this.detailIsVisible.set(this.detailIsVisible());
+    this.detailIsVisible.update(wasVisible => !wasVisible);
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
+  }
 }
