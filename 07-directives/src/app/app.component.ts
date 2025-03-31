f892ef7f95b858976a8cd99defe47c3fe0a9ea51
hshoon <hshoon@egiskorea.com>
2025-03-31 10:55:35 +0900

    7. 프로젝트트

---
+++ 07-directives/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, computed, inject } from '@angular/core';
+
+import { AuthComponent } from './auth/auth.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, AuthComponent, LearningResourcesComponent],
+})
+export class AppComponent {
+  private authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
+  
+}
